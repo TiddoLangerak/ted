afe01346c5d10c5a1396df61d4ebc2de7c7329cc
Tiddo Langerak <tiddolangerak@gmail.com>
2015-09-27 23:09:24 +0200

    Added G and gg motions

--- src/client/motions/movement.js
+++ src/client/motions/movement.js
@@ -1,7 +1,15 @@
-export default ({ window }) => ({
+export default ({ window, changeMode }) => ({
 	'h' : () => window.cursor.moveLeft(),
 	'l' : () => window.cursor.moveRight(),
 	'j' : () => window.cursor.moveDown(),
 	'k' : () => window.cursor.moveUp(),
-	'$' : window.cursor.moveToEOL
+	'$' : window.cursor.moveToEOL,
+	'G' : () => window.cursor.update(cursor => cursor.y = window.lines.length -1),
+	'g' : () => ({
+		'g' : () => {
+			window.cursor.update(cursor => cursor.y = 0);
+			return changeMode('normal');
+		},
+		default: () => changeMode('normal')
+	})
 });
