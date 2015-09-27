e697e185bbcf1873c188de9be38da68a66e56bf8
Tiddo Langerak <tiddolangerak@gmail.com>
2015-09-27 17:37:47 +0200

    Copy-pasting is now done using the system clipboard
    Because there's no need to implement that ourselves

--- src/client/motions/clipboard.js
+++ src/client/motions/clipboard.js
@@ -1,33 +1,45 @@
 import { diffTypes } from '../../diff';
-
-let clipboard = '';
-let isBlock = false;
+import { copy, paste } from 'copy-paste';
+import promisify from '../../promisify';
 
 export default ({ window, changeMode, contentManager }) => ({
 	'y' : () => {
 		return {
 			'y' : () => {
-				clipboard = window.lines[window.cursor.y];
-				isBlock = true;
+				const line = window.lines[window.cursor.y];
+				copy(line + '\n');
 				return changeMode('normal');
 			}
 		};
 	},
 	'p' : () => {
-		const diff = {
-			type : diffTypes.INSERT,
-			line : window.cursor.y,
-			column : window.cursor.x,
-			text : clipboard
-		};
-		if (isBlock) {
-			diff.column = window.lineLength(window.cursor.y);
-			diff.text = '\n' + diff.text;
-		}
-		contentManager.processClientDiff(diff);
-		if (isBlock) {
-			window.cursor.moveDown();
-		}
+		(async function() {
+			const text = await promisify(cb => paste(cb));
+			//We want to paste after the cursor, but also move the cursor to the end of the pasted text.
+			//Since inserting text *before* the cursor will move the cursor automatically we'll abuse
+			//that to position our cursor right: we'll move one character to the right, insert text there
+			//and then move one character back.
+			//
+			//We also support "block mode" pasting, i.e. pasting complete lines. These will always
+			//be placed on their own line. Text ending with a newline will be pasted in block mode
+			window.cursor.moveRight();
+			const diff = {
+				type : diffTypes.INSERT,
+				line : window.cursor.y,
+				column : window.cursor.x,
+				text
+			};
+			const isBlock = text.charAt(text.length - 1) === '\n';
+			if (isBlock) {
+				diff.column = 0;
+				diff.line++;
+			}
+			contentManager.processClientDiff(diff);
+			window.cursor.moveLeft();
+			if (isBlock) {
+				window.cursor.moveDown();
+			}
+		}());
 		return changeMode('normal');
 	},
 	default : () => changeMode('normal')
